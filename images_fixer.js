bc7b1be24becf422f61d639eeccbd9b5f12b91c4
Loïc <59501884+LoicE5@users.noreply.github.com>
2021-06-02 11:46:57 +0200

    Update images_fixer.js

--- images_fixer.js
+++ images_fixer.js
@@ -1,3 +1,8 @@
+/* 
+	Paramètres à modifier :
+	dirName (laisser vide en cas d'accès via le nom de domaine, ou mettre "ojs2" en cas d'accès via l'IP)
+	journalName
+*/
 const dirName = "<your_ojs_directory_name>";
 const journalName = "<your_journal_name>";
 
@@ -5,13 +10,33 @@ const DCIdentifier = document.querySelector('meta[name="DC.Identifier"]');
 const imgs = document.querySelectorAll('#jatsParserFullText img');
 
 const submissionId = Number(DCIdentifier.getAttribute('content'));
-const fileId = syncFetch(`/betterOJS/file_id_finder.php?submissionId=${submissionId}&expected=xml`);
 const stageId = 5;
 
+let fileId;
+if(dirName == "" || dirName == null || dirName == undefined){
+    fileId = syncFetch(`/public/journals/4/betterOJS/file_id_finder.php?submissionId=${submissionId}&expected=xml`);
+} else {
+    fileId = syncFetch(`/${dirName}/public/journals/4/betterOJS/file_id_finder.php?submissionId=${submissionId}&expected=xml`);
+}
+
 imgs.forEach(img=>{
+
     const src = img.getAttribute('src');
-    const imageId = syncFetch(`/betterOJS/file_id_finder.php?submissionId=${submissionId}&expected=img&imageName=${src}`);
-    let newUrl = `/${dirName}/index.php/${journalName}/article/downloadFullTextAssoc/${submissionId}/${fileId}/${imageId}`;
+    let imageId;
+    let newUrl;
+
+    if(dirName == "" || dirName == null || dirName == undefined){
+
+        imageId = syncFetch(`/public/journals/4/betterOJS/file_id_finder.php?submissionId=${submissionId}&expected=img&imageName=${src}`);
+        newUrl = `/index.php/${journalName}/article/downloadFullTextAssoc/${submissionId}/${fileId}/${imageId}`;
+
+    } else {
+
+        imageId = syncFetch(`/${dirName}/public/journals/4/betterOJS/file_id_finder.php?submissionId=${submissionId}&expected=img&imageName=${src}`);
+        newUrl = `/${dirName}/index.php/${journalName}/article/downloadFullTextAssoc/${submissionId}/${fileId}/${imageId}`;
+
+    }
+
     img.src = newUrl;
 });
 
@@ -35,3 +60,4 @@ function syncFetch(url){
     }
     return response;
 }
+
